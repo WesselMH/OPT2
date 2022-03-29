41602226bbf818b5672f53c20baf84f0639a687e
WesselMH <w.m.horsthuis@live.nl>
2022-03-29 15:57:53 +0200

    Gezinsleden

--- src/main/java/Kind.java
+++ src/main/java/Kind.java
@@ -1,2 +1,22 @@
-public class Kind {
+import java.util.ArrayList;
+
+public class Kind extends Gezin {
+    //private String naam;
+    //private double beschikbareTijd;
+    public ArrayList<Gezin> kind = new ArrayList<>();
+
+    public void kind(String naam, double beschikbareTijd){
+        super(naam, beschikbareTijd);
+        kind.add(this);
+        
+    }
+
+    public String getNaam(){
+        return this.naam;
+    }
+
+    public double getBeschikbareTijd() {
+        return this.beschikbareTijd;
+    }
+
 }
--- src/main/java/Ouder.java
+++ src/main/java/Ouder.java
@@ -1,2 +1,22 @@
-public class Ouder {
+import java.util.ArrayList;
+
+public class Ouder extends Gezin {
+    //private String naam;
+    //private double beschikbareTijd;
+    public ArrayList<Gezin> ouder = new ArrayList<>();
+
+    
+    public void ouderGezinslid(String naam, double beschikbareTijd){
+        super(naam, beschikbareTijd);
+        ouder.add(this);
+        
+    }
+
+    public String getNaam(){
+        return this.naam;
+    }
+
+    public double getBeschikbareTijd() {
+        return this.beschikbareTijd;
+    }
 }
